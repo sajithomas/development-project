793b493adde65ec608052794254943e481b6e733
sajithomas <thomas.saji07@gmail.com>
2017-02-28 18:26:35 -0500

    assignment 2

--- app.js
+++ app.js
@@ -1,4 +1,70 @@
-function Home() {
+function goBack() {
     window.history.back();
 }
 
+$(document).ready(function()
+		{
+	
+	 $("#btn1").click(function(){
+	        $(".button").animate({height: "100px"});
+	    });
+	
+			$(".button").click( function()
+			        {
+			          alert('button clicked');
+			        }
+			 );
+			
+		});
+
+$(document).ready(function(){
+	$(".imagesContainer").click(function(){
+		alert("Image Clicked");
+	});	
+});
+
+$(document).ready(function(){
+    $("#resume").click(function(){
+        $("#marq").toggleClass("main");
+    });
+});
+
+
+$('div').each(function (index, value) { 
+	  console.log($(this).attr('id')); 
+	});
+
+$(document).ready(function(){
+	$("#resume").hover(function(){
+		$("#resume").html("Click here to download Resume");		
+	})
+
+});
+
+$('button#resume').click(function() {
+
+	document.getElementById(draggable).style.visibility = "visible";
+	document.getElementById(droppable).style.visibility = "visible";
+
+});
+
+$( init );
+function init() {
+	$('#draggable').draggable();
+	$('#droppable').droppable( {
+		drop: handleDropEvent
+	} );
+}
+
+function handleDropEvent( event, ui ) {
+	var draggable = ui.draggable;
+
+	location.href = './resume.pdf';
+	// alert( 'Dropped' );
+}
+
+/*$("#resume" ).droppable({
+	  drop: function( event, ui ) {}
+	});*/
+
+//$("#resume" ).on( "drop", function( event, ui ) {} );
